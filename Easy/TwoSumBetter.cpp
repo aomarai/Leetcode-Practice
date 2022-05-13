7bc504cc9b0fe06e731375b1250dad88197a6a8a
aomarai <49348213+aomarai@users.noreply.github.com>
2022-05-12 19:09:29 -0500

    Tried using unordered map

--- Easy/TwoSumBetter.cpp
+++ Easy/TwoSumBetter.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution
@@ -8,6 +9,25 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        //Better approach
+        //Better approach using an unordered map
+        unordered_map<int, int> map;
+        int elementToFind;
+
+        //Insert all elements from nums into the map
+        for (int i = 0; i < nums.size(); i++)
+        {
+            map.insert({nums[i], i});
+        }
+
+        //Iterate through the map and find the element to find
+        for (int i = 0; i < nums.size(); i++)
+        {
+            elementToFind = target - nums[i];
+            if (map.find(elementToFind) != map.end() && map[elementToFind] != i)
+            {
+                return {i, map[elementToFind]};
+            }
+        }
+        return {-1, -1};
     }
 };
