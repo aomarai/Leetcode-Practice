7bd7d38080f90cb3349974eea3269dfe520344a9
aomarai <49348213+aomarai@users.noreply.github.com>
2022-05-16 09:44:42 -0500

    Did a few questions

--- Study Plans/Algorithms 1/35SearchInsertPosition.cpp
+++ Study Plans/Algorithms 1/35SearchInsertPosition.cpp
@@ -6,6 +6,12 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+        int min = 0, max = nums.size();
+        int mid;
+
+        while (min <= max)
+        {
+            
+        }
     }
 };
